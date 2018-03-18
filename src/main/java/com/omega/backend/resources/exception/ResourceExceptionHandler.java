ebadaf3a9d9049bf63d0175a60a5e6def46b3679
Fabio Abreu <fnmabreu@gmail.com>
2018-03-18 17:55:37 +0000

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/omega/backend/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.omega.backend.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.omega.backend.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
--- src/main/java/com/omega/backend/services/CategoriaService.java
+++ src/main/java/com/omega/backend/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.omega.backend.domain.Categoria;
 import com.omega.backend.repositories.CategoriaRepository;
+import com.omega.backend.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,8 +18,8 @@ public class CategoriaService {
 	public Categoria getCategoria(Integer id) {
 
 		Optional<Categoria> obj = repo.findById(id);
-
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
 }
