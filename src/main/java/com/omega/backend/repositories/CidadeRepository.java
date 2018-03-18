d815bbecc573531f2ac5e116c9c8cf25fb344f8f
Fabio Abreu <fnmabreu@gmail.com>
2018-03-18 18:23:29 +0000

    Estado e Cidade

---
+++ src/main/java/com/omega/backend/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.omega.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.omega.backend.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
