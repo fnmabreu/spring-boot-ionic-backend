30808fd4e99e0152cdfd747341cf0767e3d4cfb3
Fabio Abreu <fnmabreu@gmail.com>
2018-03-22 21:33:57 +0000

    Validação sintática com Bean Validation

--- src/main/java/com/omega/backend/services/CategoriaService.java
+++ src/main/java/com/omega/backend/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.omega.backend.domain.Categoria;
+import com.omega.backend.dto.CategoriaDTO;
 import com.omega.backend.repositories.CategoriaRepository;
 import com.omega.backend.services.exception.DataIntegrityException;
 import com.omega.backend.services.exception.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
