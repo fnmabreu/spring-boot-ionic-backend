b505ebc563588d087c14769cf26da7f3092177be
Fabio Abreu <fnmabreu@gmail.com>
2018-03-22 22:43:06 +0000

    PUT, DELETE e GET para Cliente

--- src/main/java/com/omega/backend/services/ClienteService.java
+++ src/main/java/com/omega/backend/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.omega.backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.omega.backend.domain.Cliente;
+import com.omega.backend.dto.ClienteDTO;
 import com.omega.backend.repositories.ClienteRepository;
+import com.omega.backend.services.exception.DataIntegrityException;
 import com.omega.backend.services.exception.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,39 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível eliminar um Cliente que possui pedidos");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
