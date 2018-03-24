6c0666ed7fcc19a41f4ae0e38e6af8edce99de63
Fabio Abreu <fnmabreu@gmail.com>
2018-03-24 17:04:21 +0000

    Inserindo um novo Pedido

--- src/main/java/com/omega/backend/domain/PagamentoComCartao.java
+++ src/main/java/com/omega/backend/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.omega.backend.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.omega.backend.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/omega/backend/services/PedidoService.java
+++ src/main/java/com/omega/backend/services/PedidoService.java
@@ -1,12 +1,19 @@
 package com.omega.backend.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.omega.backend.domain.ItemPedido;
+import com.omega.backend.domain.PagamentoComBoleto;
 import com.omega.backend.domain.Pedido;
+import com.omega.backend.domain.enums.EstadoPagamento;
+import com.omega.backend.repositories.ItemPedidoRepository;
+import com.omega.backend.repositories.PagamentoRepository;
 import com.omega.backend.repositories.PedidoRepository;
+import com.omega.backend.repositories.ProdutoRepository;
 import com.omega.backend.services.exception.ObjectNotFoundException;
 
 @Service
@@ -15,6 +22,18 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepo;
+	
+	@Autowired
+	private ProdutoRepository produtoRepo;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepo;
+
 	public Pedido find(Integer id) {
 
 		Optional<Pedido> obj = repo.findById(id);
@@ -22,4 +41,35 @@ public class PedidoService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 
 	}
+
+	public Pedido insert(Pedido obj) {
+
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		//Save do pedido
+		obj = repo.save(obj); 
+		
+		//Save do Pagamento
+		pagamentoRepo.save(obj.getPagamento()); 
+		
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoRepo.findById(ip.getProduto().getId()).orElse(null).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		//Save dos Itens
+		itemPedidoRepo.saveAll(obj.getItens());
+		
+		return obj;
+	}
+	
 }
