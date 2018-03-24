f17f22d85d6cb9b788f1d8a177bf9e695cad2ea8
Fabio Abreu <fnmabreu@gmail.com>
2018-03-24 13:53:04 +0000

    Validação customizada: email nao repetido na atualização do Cliente

--- src/main/java/com/omega/backend/dto/ClienteDTO.java
+++ src/main/java/com/omega/backend/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.omega.backend.domain.Cliente;
+import com.omega.backend.services.validation.ClientUpdate;
 
+@ClientUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
