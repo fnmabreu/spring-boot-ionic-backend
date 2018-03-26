54760108821da5e48eb9bd226a136df9565b5004
Fabio Abreu <fnmabreu@gmail.com>
2018-03-26 23:20:26 +0100

    Configuração inicial do Spring Security

--- src/main/java/com/omega/backend/repositories/ProdutoRepository.java
+++ src/main/java/com/omega/backend/repositories/ProdutoRepository.java
@@ -17,6 +17,11 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	@Transactional(readOnly = true)
 	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
 			Pageable pageRequest);
-	
-	
+
+	/*
+	 * Fazendo override da query.
+	 * 
+	 * @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias"
+	 * ) Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	 */
 }
--- src/main/java/com/omega/backend/resources/utils/URL.java
+++ src/main/java/com/omega/backend/resources/utils/URL.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class URL {
-	
+
 	public static String decodeParam(String s) {
 		try {
 			return URLDecoder.decode(s, "UTF-8");
@@ -16,22 +16,19 @@ public class URL {
 		}
 	}
 
+
 	public static List<Integer> decodeIntList(String s) {
 
 		return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
 	}
 
-
 	/*
 	public static List<Integer> decodeIntList(String s) {
-
 		String[] vet = s.split(",");
 		List<Integer> list = new ArrayList<>();
-
 		for (int i = 0; i < vet.length; i++) {
 			list.add(Integer.parseInt(vet[i]));
 		}
-
 		return list;
 	}
 	*/
