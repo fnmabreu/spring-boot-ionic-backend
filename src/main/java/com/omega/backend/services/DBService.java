77858a4d6e2104d42b009744cf48639564b5d371
Fabio Abreu <fnmabreu@gmail.com>
2018-03-26 20:46:56 +0100

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/omega/backend/services/DBService.java
+++ src/main/java/com/omega/backend/services/DBService.java
@@ -118,7 +118,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "228374733", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Fabio Abreu", "fnmabreu@gmail.com", "228374733", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("221823848", "218372888"));
 
