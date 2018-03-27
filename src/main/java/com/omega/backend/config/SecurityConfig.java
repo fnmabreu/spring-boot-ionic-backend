765d19359dcd3a9c7cc979602a64bdde1a0d671c
Fabio Abreu <fnmabreu@gmail.com>
2018-03-27 23:28:11 +0100

    Implementação da autenticação e geração do token JWT

--- src/main/java/com/omega/backend/config/SecurityConfig.java
+++ src/main/java/com/omega/backend/config/SecurityConfig.java
@@ -7,48 +7,63 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.omega.backend.security.JWTAuthenticationFilter;
+import com.omega.backend.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-    private Environment env;
+	private JWTUtil jwtUtil;
 	
-	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
-	};
-	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private Environment env;
+
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
+
 	/**
-	 * Definiçao dos caminhos dos endpoints que vão estar acessiveis 
+	 * Definiçao dos caminhos dos endpoints que vão estar acessiveis
 	 */
-	private static final String[] PUBLIC_MATCHERS_GET = { 
-			"/produtos/**", 
-			"/categorias/**",
-			"/clientes/**"
-	};
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**", "/clientes/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
-            http.headers().frameOptions().disable();
-        }
-		
+			http.headers().frameOptions().disable();
+		}
+
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	/**
+	 * Definição do userDetailsService e passwordEncoder
+	 * 
+	 */
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	/**
---
+++ src/main/java/com/omega/backend/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.omega.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.omega.backend.domain.Cliente;
+import com.omega.backend.repositories.ClienteRepository;
+import com.omega.backend.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = clienteRepo.findByEmail(email);
+
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
