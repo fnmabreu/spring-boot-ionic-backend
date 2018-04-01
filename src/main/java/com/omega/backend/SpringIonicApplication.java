be1e92fb60f5479101248184e30eb5ae1de44b13
Fabio Abreu <fnmabreu@gmail.com>
2018-04-01 22:15:31 +0100

    Salvando primeiro arquivo no S3

--- src/main/java/com/omega/backend/SpringIonicApplication.java
+++ src/main/java/com/omega/backend/SpringIonicApplication.java
@@ -1,11 +1,17 @@
 package com.omega.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.omega.backend.services.S3Service;
+
 @SpringBootApplication
 public class SpringIonicApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
@@ -13,6 +19,8 @@ public class SpringIonicApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		String path = System.getProperty("user.home") + "/Downloads/cat1.jpg";
+		s3Service.uploadFile(path);
 
 	}
 }
---
+++ src/main/java/com/omega/backend/config/S3Config.java
@@ -0,0 +1,40 @@
+package com.omega.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+@PropertySource(value = "file://${HOME}/.springionic_S3.properties", ignoreResourceNotFound = true)
+public class S3Config {
+
+	@Autowired
+	private Environment env;
+
+	private String awsId;
+
+	private String awsKey;
+
+	@Value("${s3.region}")
+	private String region;
+
+	@Bean
+	public AmazonS3 s3client() {
+		awsId = env.getProperty("aws.access_key_id");
+		awsKey = env.getProperty("aws.secret_access_key");
+
+		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsId, awsKey);
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+				.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+		return s3client;
+	}
+}
